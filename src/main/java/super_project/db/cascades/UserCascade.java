68d79516f6232b7eae28ab36522a2cc7c8de53ea
Mykhailo <mhorodnyi@outlook.com>
2020-08-02 00:18:55 +0300

    Added cascade

--- src/main/java/super_project/db/cascades/UserCascade.java
+++ src/main/java/super_project/db/cascades/UserCascade.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
-import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
 
 import super_project.db.models.User;
 
@@ -12,8 +11,8 @@ public class UserCascade extends AbstractMongoEventListener<Object> {
     @Autowired
     private MongoOperations mongoOperations;
  
-    @Override
-    public void onBeforeSave(BeforeSaveEvent<Object> event) {
+   @Override
+    public void onBeforeConvert(BeforeConvertEvent<Object> event) {
         Object source = event.getSource();
         
         if (source instanceof User) {
@@ -25,17 +24,4 @@ public class UserCascade extends AbstractMongoEventListener<Object> {
             }
         }
     }
-//   @Override
-//    public void onBeforeConvert(BeforeConvertEvent<Object> event) {
-//        Object source = event.getSource();
-//        
-//        if (source instanceof User) {
-//        	if (((User) source).getAddress() != null) {
-//        		mongoOperations.save(((User) source).getAddress());
-//        	}
-//            if (((User) source).getContacts() != null) {
-//                mongoOperations.save(((User) source).getContacts());
-//            }
-//        }
-//    }
 }
